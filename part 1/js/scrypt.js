3d0ad03b868e99a7af6f08f08250ec12e38ea4b3
SviatoslavK <kolodaslav@gmail.com>
2016-07-30 12:27:25 +0300

    added js homework

---
+++ part 1/js/scrypt.js
@@ -0,0 +1,12 @@
+
+function involution(a, b) {
+    var result = Math.pow(a, b);
+    console.log('involution = ', result);
+    return result;
+}
+
+var a = prompt('Please, enter number...');
+
+var b = prompt('And exponent...');
+
+involution(a, b);
