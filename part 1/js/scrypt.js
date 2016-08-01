8dde4b25fcc0406b11a42de048421f5fbe071d85
SviatoslavK <kolodaslav@gmail.com>
2016-08-01 10:12:58 +0300

    correct script

--- part 1/js/scrypt.js
+++ part 1/js/scrypt.js
@@ -1,12 +1,21 @@
-
 function involution(a, b) {
-    var result = Math.pow(a, b);
+    
+    var result = 1;
+    for(var i = 0; i < b; i++) {
+    result *= a 
+    
+    };
+    
     console.log('involution = ', result);
     return result;
-}
+};
 
 var a = prompt('Please, enter number...');
 
 var b = prompt('And exponent...');
 
 involution(a, b);
+
+
+
+
